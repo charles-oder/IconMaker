9a8d7b1a4e309e25bda6b202290641d2deadd265
Charles Oder <charles@oder.us>
2017-08-24 20:52:11 -0500

    Initial Commit

---
+++ IconMaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IconMaker
+//
+//  Created by Charles Oder on 8/24/17.
+//  Copyright © 2017 Charles Oder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
